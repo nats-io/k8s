c2a9807845853832555f79efd9b6930b3a87065f
Jaime Piña <jaime@synadia.com>
2019-11-05 13:35:44 -0800

    Add kubectl commands

--- setup/nats-setup.sh
+++ setup/nats-setup.sh
@@ -16,3 +16,21 @@ nsc add user --name test
   nsc generate config --mem-resolver --sys-account SYS > resolver.conf
 )
 chown -R 1000:1000 /nsc
+
+kubectl create secret generic nats-sys-creds  --from-file /nsc/nkeys/creds/KO/SYS/sys.creds
+kubectl create configmap nats-accounts --from-file /nsc/config/resolver.conf
+
+# Install NATS Server
+kubectl apply -f 'https://raw.githubusercontent.com/nats-io/nats.k8s/sts/nats-server/nats-server-v2-external.yml?token=AAAGMU3HLE3S3MWJBZRWYOK5ZI6IQ'
+
+# Install Prometheus Operator
+kubectl apply -f 'https://raw.githubusercontent.com/nats-io/nats.k8s/sts/nats-server/prometheus-operator.yml?token=AAAGMU2LN4MDGEOS3XSTBSK5ZI6PQ'
+
+# Create Prometheus instance for NATS usage
+kubectl apply -f 'https://raw.githubusercontent.com/nats-io/nats.k8s/sts/nats-server/nats-prometheus.yml?token=AAAGMU43WUQHUYIJKH2CS7S5ZI6RO'
+
+# Deploy NATS Surveyor
+kubectl apply -f 'https://raw.githubusercontent.com/nats-io/nats.k8s/sts/nats-server/nats-surveyor.yaml?token=AAAGMU756KJWFQVKGXVBGWS5ZI6TK'
+
+# Deploy NATS Surveyor Grafana instance
+kubectl apply -f 'https://raw.githubusercontent.com/nats-io/nats.k8s/sts/nats-server/nats-surveyor-grafana.yaml?token=AAAGMU5R46ML2KBTYBTQZWC5ZI6TS'
