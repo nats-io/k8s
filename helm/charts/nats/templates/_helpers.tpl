nats-io/k8s

commit 9d0bdc73c058bcd07a7c440d176b979ac41a6cf7

Fix for clusterRoutes template generation, host names are generated with the same pattern as the one for the statefulset

helm/charts/nats/templates/_helpers.tpl
@@ -58,7 +58,7 @@ Return the proper NATS image name
 Return the NATS cluster routes.
 */}}
 {{- define "nats.clusterRoutes" -}}
-{{- $name := default .Release.Name .Values.nameOverride | trunc 63 | trimSuffix "-" -}}
+{{- $name := = (include "nats.fullname" . ) -}}
 {{- range $i, $e := until (.Values.cluster.replicas | int) -}}
 {{- printf "nats://%s-%d.%s.%s.svc:6222," $name $i $name $.Release.Namespace -}}
 {{- end -}}

commit 0457408b73971233c4bb01a20caf769fb8b444e3

fix: broken gotmpl
regresion from 9d0bdc73c058bcd07a7c440d176b979ac41a6cf7

helm/charts/nats/templates/_helpers.tpl
@@ -58,7 +58,7 @@ Return the proper NATS image name
 Return the NATS cluster routes.
 */}}
 {{- define "nats.clusterRoutes" -}}
-{{- $name := = (include "nats.fullname" . ) -}}
+{{- $name := (include "nats.fullname" . ) -}}
 {{- range $i, $e := until (.Values.cluster.replicas | int) -}}
 {{- printf "nats://%s-%d.%s.%s.svc:6222," $name $i $name $.Release.Namespace -}}
 {{- end -}}